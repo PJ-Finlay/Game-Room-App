44e91adbd3af98a85d09f73699511f39cb6d6fe6
P.J. Finlay <pjappdevelopment@gmail.com>
2015-12-31 17:29:01 -0500

    Setup Debugging

---
+++ debug/unittests/testdirectory.h
@@ -0,0 +1,23 @@
+#ifndef TESTDIRECTORY_H
+#define TESTDIRECTORY_H
+
+/**
+ * @brief The TestDirectory class provides links to blocks of unit tests. The primary method of this class is allTests(), which tests all of the currently written unit tests.
+ * There are also methods for testing other blocks of unit tests.
+ */
+class TestDirectory
+{
+public:
+    /**
+     * @brief Runs every currently written unit test.
+     */
+    static void allTests();
+
+    /**
+     * @brief Runs all of the unit tests for game states
+     * @return If all of the tests were passed
+     */
+    static bool allGameStates();
+};
+
+#endif // TESTDIRECTORY_H
