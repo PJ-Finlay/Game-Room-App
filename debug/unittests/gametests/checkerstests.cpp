bc9c5c90a98baee1f87242c7067a9d9e8310f89a
P.J. Finlay <pjappdevelopment@gmail.com>
2016-01-23 18:05:46 -0500

    Implemented CheckersGameState::findValidMoves

--- debug/unittests/gametests/checkerstests.cpp
+++ debug/unittests/gametests/checkerstests.cpp
@@ -21,16 +21,129 @@ bool CheckersTests::testCheckersGameState()
 
     CheckersGameState g;
 
+    //Test getTurn
     if(g.getTurn() != 1){
         allTestsPassed = false;
         qDebug() << "Getting or setting turn not working correctly";
     }
 
+    //Test initialize game
     g.initializeGame();
-    if(g.getGameState().compare("________________________________________________________________") !=0 ){
+    if(g.getGameState().compare("________________________________________________________________1") !=0 ){
         allTestsPassed = false;
         qDebug() << "Initialize Game or getGameState not working correctly";
     }
+
+    //Test setGameState and getGameState
+    g.setGameState(QString("____________________________r______b_____r_b____________________1"));
+    if(g.getGameState().compare("____________________________r______b_____r_b____________________1") != 0){
+        allTestsPassed = false;
+        qDebug() << "setGameState or getGamestate not working";
+    }
+    g.setGameState(QString("____________________________r______b_____r_b____________________2"));
+    if(g.getGameState().compare("____________________________r______b_____r_b____________________2") != 0){
+        allTestsPassed = false;
+        qDebug() << "setGameState or getGamestate not working";
+    }
+    g.setGameState(QString("b___________________________r______b_____r_b____________________1"));
+    if(g.getGameState().compare("b___________________________r______b_____r_b____________________1") != 0){
+        allTestsPassed = false;
+        qDebug() << "setGameState or getGamestate not working";
+    }
+    g.setGameState(QString("____________________________r______b_____r_b___________________r1"));
+    if(g.getGameState().compare("____________________________r______b_____r_b___________________r1") != 0){
+        allTestsPassed = false;
+        qDebug() << "setGameState or getGamestate not working";
+    }
+
+    //Testing findValidMoves
+    g.initializeGame();
+    if(g.findValidMoves().join(",").compare("") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("________________________________________________________b_______1");
+    if(g.findValidMoves().join(",").compare("0011") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("_______________________________________________________________b1");
+    if(g.findValidMoves().join(",").compare("7061") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("______________________________________________________r________b1");
+    if(g.findValidMoves().join(",").compare("7052") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("______________________________________________________r________b2");
+    if(g.findValidMoves().join(",").compare("6150") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("b_______________________________________________________________2");
+    if(g.findValidMoves().join(",").compare("") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("b_______________________________________________________________1");
+    if(g.findValidMoves().join(",").compare("") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("r_______________________________________________________________1");
+    if(g.findValidMoves().join(",").compare("") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("r_______________________________________________________________2");
+    if(g.findValidMoves().join(",").compare("0716") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("r________b______________________________________________________2");
+    if(g.findValidMoves().join(",").compare("0725") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("r________b______________________________________________________1");
+    if(g.findValidMoves().join(",").compare("1627") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("_______r________________________________________________________2");
+    if(g.findValidMoves().join(",").compare("7766") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("_______r______b_________________________________________________2");
+    if(g.findValidMoves().join(",").compare("7755") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("_____________________b_____________________r____________________1");
+    QStringList validMoves = g.findValidMoves();
+    if(!validMoves.contains("5566") || !validMoves.contains("5546")){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    g.setGameState("_____________________b_____________________r____________________2");
+    validMoves = g.findValidMoves();
+    if(!validMoves.contains("3221") || !validMoves.contains("3241")){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+    //Do tests with kings
+    g.setGameState("B_______________________________________________________________1");
+    if(g.findValidMoves().join(",").compare("0716") != 0){
+        allTestsPassed = false;
+        qDebug() << "findValidMoves not working correctly";
+    }
+
+
+    g.printGameState();
+    qDebug() << g.findValidMoves().join(",");
 
     return allTestsPassed;
 }
--- gamestate/individual_game_states/checkersgamestate.cpp
+++ gamestate/individual_game_states/checkersgamestate.cpp
@@ -3,6 +3,7 @@
 #include <QChar>
 #include <QDebug>
 #include <string>
+#include <iostream>
 
 CheckersGameState::CheckersGameState() : GameState()
 {
@@ -25,26 +26,15 @@ QString CheckersGameState::getGameState() const{
             toReturn.append(board[x][y]);
         }
     }
+    toReturn.append(QString::number(playerTurn));
     return toReturn;
 }
 
 void CheckersGameState::setGameState(QString gameState){
-    for(int i = 0; i < 9; i++){ //loop through the gameState string's characters
-        board[i % 3][2 - (i / 3)] = gameState.at(i).toLatin1();
+    for(int i = 0; i < 64; i++){ //loop through the gameState string's characters
+        board[i % 8][7 - (i / 8)] = gameState.at(i).toLatin1();
     }
-
-    //Figure out which player's turn it is
-    int xCount = 0;
-    int oCount = 0;
-    for(int i = 0; i < 9; i++){ //loop through the gameState string's characters
-        if(gameState.at(i).toLatin1() == 'X') xCount++;
-        if(gameState.at(i).toLatin1() == 'O') oCount++;
-    }
-    if(xCount == oCount){
-        playerTurn = 1;
-    }else{
-        playerTurn = 2;
-    }
+    playerTurn = gameState.right(1).toInt();
 
 }
 
@@ -157,21 +147,82 @@ int CheckersGameState::getTurn() const
 
 QStringList CheckersGameState::findValidMoves() const
 {
+    char regTurn = 'b';
+    char kingTurn = 'B';
+    if(playerTurn == 2){
+        regTurn = 'r';
+        kingTurn = 'R';
+    }
+
     QStringList toReturn;
-    for(int i = 0; i < 3; i++){ //Loop through x coordinates of the board
-        for(int j = 0; j < 3; j++){//Loop through the y coordinates of the board
-            if(board[i][j] == '_'){
-                QString xString = QString::number(i);
-                QString yString = QString::number(j);
-                toReturn.append(xString.append(yString));
+    for(int y = 7; y >=0; y--){
+        for(int x = 0; x < 8; x++){
+            if(board[x][y] != '_' && (board[x][y] == regTurn || board[x][y] == kingTurn)){
+                toReturn += validMovesForPiece(x,y);
             }
         }
     }
     return toReturn;
 }
 
+QStringList CheckersGameState::validMovesForPiece(int x, int y) const
+{
+    QStringList toReturn;
+
+    int direction = 1;
+    if(board[x][y] == 'r' || board[x][y] == 'R'){
+        direction = -1;
+    }
+
+
+    //This uses short circuit evaluation to prevent out of bounds errors
+    //For all pieces
+    if(x > 0 && y+direction>=0 && y+direction<=7 && board[x-1][y+1*direction] == '_'){ //Checks up/left
+        toReturn.append(QString::number(x) + QString::number(y) + QString::number(x-1) + QString::number(y+1*direction));
+    }
+    if(x < 7 && y+direction>=0 && y+direction<=7 && board[x+1][y+1*direction] == '_'){ //Checks up/right
+        toReturn.append(QString::number(x) + QString::number(y) + QString::number(x+1) + QString::number(y+1*direction));
+    }
+    //Jumps
+    if(x > 0 && y+direction*2>=0 && y+direction*2<=7 && board[x-2][y+2] == '_'){ //Checks up/left for jump
+        if(QString(board[x][y]).toLower().compare(QString(board[x-1][y+1*direction])) != 0 && board[x-1][y+1*direction] != '_')
+            toReturn.append(QString::number(x) + QString::number(y) + QString::number(x-2) + QString::number(y+2*direction));
+    }
+    if(x < 7 &&  y+direction*2>=0 && y+direction*2<=7  && board[x+2][y+2] == '_'){ //Checks up/right for jump
+        if(QString(board[x][y]).toLower().compare(QString(board[x+1][y+1*direction])) != 0 && board[x+1][y+1*direction] != '_')
+        toReturn.append(QString::number(x) + QString::number(y) + QString::number(x+2) + QString::number(y+2*direction));
+    }
+
+    //For kings
+    if(board[x][y] == 'R' || board[x][y] == 'B'){ //If the piece is a king
+        if(x > 0 && y-direction>=0 && y-direction<=7 && board[x-1][y-1*direction] == '_'){ //Checks back/left
+            toReturn.append(QString::number(x) + QString::number(y) + QString::number(x-1) + QString::number(y-1*direction));
+        }
+        if(x < 7 && y-direction>=0 && y-direction<=7  && board[x+1][y-1*direction] == '_'){ //Checks back/right
+            toReturn.append(QString::number(x) + QString::number(y) + QString::number(x+1) + QString::number(y-1*direction));
+        }
+        //Jumps
+        if(x > 0 && y-direction*2>=0 && y-direction*2<=7  && board[x-2][y-2] == '_'){ //Checks back/left for jump
+            if(QString(board[x][y]).toLower().compare(QString(board[x-1][y-1*direction])) != 0 && board[x-1][y-1*direction] != '_')
+            toReturn.append(QString::number(x) + QString::number(y) + QString::number(x-2) + QString::number(y-2*direction));
+        }
+        if(x < 7 &&y-direction*2>=0 && y-direction*2<=7  && board[x+2][y-2] == '_'){ //Checks back/right for jump
+            if(QString(board[x][y]).toLower().compare(QString(board[x+1][y-1*direction])) != 0 && board[x+1][y-1*direction] != '_')
+            toReturn.append(QString::number(x) + QString::number(y) + QString::number(x+2) + QString::number(y-2*direction));
+        }
+    }
+
+    return toReturn;
+}
+
 void CheckersGameState::printGameState() const{
-    qDebug() << board[0][2] << board[1][2] << board[2][2];
-    qDebug() << board[0][1] << board[1][1] << board[2][1];
-    qDebug() << board[0][0] << board[1][0] << board[2][0];
+
+    for(int y = 7; y >=0; y--){
+        QString line;
+        for(int x = 0; x < 8; x++){
+            line.append(board[x][y]);
+            line.append(" ");
+        }
+        std::cout << line.toStdString() << std::endl;
+    }
 }
