03393a9acd64e3417d6b6629838fff7f55b8091a
P.J. Finlay <pjappdevelopment@gmail.com>
2016-02-05 10:26:38 +0000

    Worked on GUI

--- ui/widgets/gamechooserbutton.cpp
+++ ui/widgets/gamechooserbutton.cpp
@@ -1,12 +1,28 @@
 #include "gamechooserbutton.h"
 
+#include <QLabel>
+#include <QVBoxLayout>
+
 GameChooserButton::GameChooserButton(std::shared_ptr<Game> game, QWidget* parent) : QPushButton(parent)
 {
+    //Create the layout for the button
+    QVBoxLayout* layout = new QVBoxLayout(this);
+
     //Assign game piv to game parameter
     this->game = game;
 
-    //Set the text of the button to the name of the game
-    this->setText(game->getName());
+    //Construct the text fo the button
+    QString name = "<h2>";
+    name.append(game->getName());
+    name.append("</h2>");
+
+    //Put the text in a QLabel
+    QLabel* title = new QLabel(name, this);
+    title->setAlignment(Qt::AlignCenter);
+
+    //Add QLabel to layout and set the layout
+    layout->addWidget(title);
+    this->setLayout(layout);
 
     //Connect the clicked signal from this button to its chooseGame slot
     QObject::connect(this,SIGNAL(clicked()),this,SLOT(chooseGame()));
