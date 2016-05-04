57560b32217ee07dc01211a77a59f4c2f4f8117f
P.J. Finlay <pjfin123@gmail.com>
2016-05-04 09:44:44 +0000

    Created PieceBasedGameWidget structure, and reimplemented TicTacToeGameWidget using it

---
+++ ui/widgets/game_widgets/game_elements/general/piecebasedgamewidget.h
@@ -0,0 +1,11 @@
+#ifndef PIECEBASEDGAMEWIDGET_H
+#define PIECEBASEDGAMEWIDGET_H
+
+
+class PieceBasedGameWidget : public GameWidget
+{
+public:
+    PieceBasedGameWidget();
+};
+
+#endif // PIECEBASEDGAMEWIDGET_H
