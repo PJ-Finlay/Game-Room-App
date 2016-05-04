24c167e577bf5f9bf2ae2b75cc7b6ff2fddc3137
P.J. Finlay <pjappdevelopment@gmail.com>
2016-05-04 09:44:44 +0000

    Created PieceBasedGameWidget structure, and reimplemented TicTacToeGameWidget using it

---
+++ ui/widgets/game_widgets/game_elements/general/piece.h
@@ -0,0 +1,11 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+
+class Piece : public QGraphicsItem
+{
+public:
+    Piece();
+};
+
+#endif // PIECE_H
---
+++ ui/widgets/game_widgets/game_elements/general/piecebasedgamewidget.cpp
@@ -0,0 +1,6 @@
+#include "piecebasedgamewidget.h"
+
+PieceBasedGameWidget::PieceBasedGameWidget()
+{
+
+}
